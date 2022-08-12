90d5a917e68254e5745af28b22595b17d765fdf6
lucaoropallo <luca.oropallo.pc@gmail.com>
2022-08-12 16:43:17 +0200

    feat(mouse): mouse lock at start

---
+++ Assets/Scrips/MouseLock.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
