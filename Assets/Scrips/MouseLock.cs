2322e246e79308c99ea3e2eaa1858e35584eaf12
lucaoropallo <luca.oropallo.pc@gmail.com>
2022-08-16 17:47:41 +0200

    feat(mouse): generic mouse lock

--- Assets/Scrips/MouseLock.cs
+++ Assets/Scrips/MouseLock.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class MouseLock : MonoBehaviour
 {
+    [SerializeField]
+    private CursorLockMode lockState = CursorLockMode.Locked;
+
+    [SerializeField]
+    private bool visible = false;
+
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.lockState = lockState;
+        Cursor.visible = visible;
     }
 }
